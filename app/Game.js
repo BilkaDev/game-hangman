44c615bdab02db7e161c501617615d2add8d6482
Marcin Bilka <bilkadev@gmail.com>
2022-01-23 22:39:33 +0100

    add play again

--- app/Game.js
+++ app/Game.js
@@ -2,6 +2,8 @@ import { Quote } from "./Quote.js";
 
 
 class Game {
+  currentStep = 0;
+  lastStep = 7;
   quotes = [
     { text: "pan tadeusz", category: "utwór literacki" },
     { text: "akademia pana kleksa", category: "Film" },
@@ -10,21 +12,32 @@ class Game {
 
 
   constructor({ lettersWrapper, categoryWrapper, wordWrapper, outputWrapper }) {
-    (this.lettersWrapper = lettersWrapper),
-      (this.categoryWrapper = categoryWrapper),
-      (this.wordWrapper = wordWrapper),
+    (this.lettersWrapper = lettersWrapper);
+      (this.categoryWrapper = categoryWrapper);
+      (this.wordWrapper = wordWrapper);
       (this.outputWrapper = outputWrapper);
 
+    this.radnomQuotes();
+
+  }
+  radnomQuotes(){
     const {text, category} = this.quotes[Math.floor(Math.random() *   this.quotes.length)];
     this.categoryWrapper.textContent = category;
     this.quote = new Quote(text)
-
   }
 
-  guess(letter) {
+  guess(letter, event) {
     event.target.disabled = true;
-    this.quote.guess(letter)
-    this.drawQuotes()
+    if (this.quote.guess(letter)){
+      this.drawQuotes();
+    } else {
+      this.currentStep++;
+      document.getElementsByClassName('step')[this.currentStep].style.opacity = '1';
+      if (this.currentStep === this.lastStep){
+        this.losing();
+      }
+    }
+
   }
   drawLetter(){
     for (let i = 0; i < 26; i++) {
@@ -39,15 +52,54 @@ class Game {
   drawQuotes() {
     const content = this.quote.getContent();
     this.wordWrapper.textContent = content
+    if (!content.includes('_')){
+      this.winning()
+      game.playAgain()
+
+    }
 
   }
 
+winning(){
+    this.wordWrapper.textContent = "Congratulations! You Win!";
+    this.lettersWrapper.textContent = ""
+}
+losing(){
+    this.wordWrapper.textContent = "Game over!";
+  game.playAgain()
 
+
+}
 
   start() {
+    document.getElementsByClassName('step')[this.currentStep].style.opacity = '1';
   this.drawLetter();
   this.drawQuotes()
 
+
+  }
+
+  playAgain() {
+    const buttonPlayAgain = document.createElement('button')
+    buttonPlayAgain.setAttribute('id','btn-again')
+    buttonPlayAgain.textContent = 'Play again'
+    buttonPlayAgain.addEventListener('click', (event) => this.restart(event))
+
+    this.lettersWrapper.textContent = "";
+    this.lettersWrapper.appendChild(buttonPlayAgain)
+    this.radnomQuotes();
+
+
+  }
+
+  restart(event) {
+    event.target.remove();
+    this.currentStep = 0;
+    [...document.getElementsByClassName('step')].forEach(item => item.style.opacity = '0.1')
+    document.getElementsByClassName('step')[this.currentStep].style.opacity = '1';
+
+    this.drawLetter();
+    this.drawQuotes()
 
   }
 }
