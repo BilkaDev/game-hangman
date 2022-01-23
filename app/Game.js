487fb550f624bccf10e6ae908ccc58f0f90f195f
Marcin Bilka <bilkadev@gmail.com>
2022-01-23 21:14:30 +0100

    class game and class quote

--- app/Game.js
+++ app/Game.js
@@ -1,12 +1,63 @@
+import { Quote } from "./Quote.js";
+
+
 class Game {
+  quotes = [
+    { text: "pan tadeusz", category: "utwór literacki" },
+    { text: "akademia pana kleksa", category: "Film" },
+    { text: "ogniem i mieczem", category: "Film" },
+  ];
 
 
-    start(){
-        console.log("lets play the game!")
+  constructor({ lettersWrapper, categoryWrapper, wordWrapper, outputWrapper }) {
+    (this.lettersWrapper = lettersWrapper),
+      (this.categoryWrapper = categoryWrapper),
+      (this.wordWrapper = wordWrapper),
+      (this.outputWrapper = outputWrapper);
 
+    const {text, category} = this.quotes[Math.floor(Math.random() *   this.quotes.length)];
+    this.categoryWrapper.textContent = category;
+    this.quote = new Quote(text)
+
+  }
+
+  guess(letter) {
+    event.target.disabled = true;
+    this.quote.guess(letter)
+    this.drawQuotes()
+  }
+  drawLetter(){
+    for (let i = 0; i < 26; i++) {
+      const label = (i + 10).toString(36);
+      const button = document.createElement("button");
+      button.textContent = label;
+      button.addEventListener("click", (event) => this.guess(label , event));
+      this.lettersWrapper.appendChild(button);
     }
+  }
+
+  drawQuotes() {
+    const content = this.quote.getContent();
+    this.wordWrapper.textContent = content
+
+  }
+
+
+
+  start() {
+  this.drawLetter();
+  this.drawQuotes()
+
+
+  }
 }
 
+const game = new Game({
+  lettersWrapper: document.getElementById('letters'),
+  categoryWrapper: document.getElementById('category'),
+  wordWrapper: document.getElementById('word'),
+  outputWrapper: document.getElementById('output')
 
-const game = new Game();
+});
+
 game.start();
